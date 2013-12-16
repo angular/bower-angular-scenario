7cc27c7b4fa0246f16f1e9c3ed5c1f00ea26c110
jenkins <ci-bot@angularjs.org>
2013-12-16 11:01:26 -0800

    v1.2.6-build.1991+sha.cbb3ce2

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.2.5
+ * @license AngularJS v1.2.6-build.1991+sha.cbb3ce2
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.5/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.6-build.1991+sha.cbb3ce2/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11621,11 +11621,11 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.5',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.6-build.1991+sha.cbb3ce2',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
-  dot: 5,
-  codeName: 'singularity-expansion'
+  dot: 6,
+  codeName: 'taco-salsafication'
 };
 
 
@@ -28675,7 +28675,7 @@ var ngRepeatDirective = ['$parse', '$animate', function($parse, $animate) {
     $$tlb: true,
     link: function($scope, $element, $attr, ctrl, $transclude){
         var expression = $attr.ngRepeat;
-        var match = expression.match(/^\s*(.+)\s+in\s+(.*?)\s*(\s+track\s+by\s+(.+)\s*)?$/),
+        var match = expression.match(/^\s*(.+)\s+in\s+([\r\n\s\S]*?)\s*(\s+track\s+by\s+(.+)\s*)?$/),
           trackByExp, trackByExpGetter, trackByIdExpFn, trackByIdArrayFn, trackByIdObjFn,
           lhs, rhs, valueIdentifier, keyIdentifier,
           hashFnLocals = {$id: hashKey};
