460b3e8ab1d4aae121ad6a7a280aecfc7cc8ac92
jenkins <ci-bot@angularjs.org>
2014-01-03 12:15:32 -0800

    v1.2.8-build.2061+sha.86151b0

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.2.8-build.2060+sha.1b7a6c6
+ * @license AngularJS v1.2.8-build.2061+sha.86151b0
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.8-build.2060+sha.1b7a6c6/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.8-build.2061+sha.86151b0/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11625,11 +11625,11 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.8-build.2060+sha.1b7a6c6',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.8-build.2061+sha.86151b0',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 8,
-  codeName: 'emoji-clairvoyance'
+  codeName: 'interdimensional-cartography '
 };
 
 
