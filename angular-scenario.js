edfb648db1abc2b81769837c5fd68edc114c35ea
jenkins <ci-bot@angularjs.org>
2014-01-04 21:06:06 -0800

    v1.2.8-build.2069+sha.1bb33cc

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.2.8-build.2068+sha.d9ed9c5
+ * @license AngularJS v1.2.8-build.2069+sha.1bb33cc
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.8-build.2068+sha.d9ed9c5/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.8-build.2069+sha.1bb33cc/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11625,7 +11625,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.8-build.2068+sha.d9ed9c5',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.8-build.2069+sha.1bb33cc',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 8,
@@ -21283,7 +21283,7 @@ function $RootScopeProvider(){
            // No digest has been run so the counter will be zero
            expect(scope.foodCounter).toEqual(0);
 
-           // Run the digest but since food has not changed cout will still be zero
+           // Run the digest but since food has not changed count will still be zero
            scope.$digest();
            expect(scope.foodCounter).toEqual(0);
 
