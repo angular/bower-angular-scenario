aee4b15efd0a78201dafdab7e9cd7a27b700a886
jenkins <ci-bot@angularjs.org>
2014-01-09 18:31:16 -0800

    v1.2.8-build.2102+sha.5a9cb8b

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.2.8-build.2101+sha.69452fa
+ * @license AngularJS v1.2.8-build.2102+sha.5a9cb8b
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.8-build.2101+sha.69452fa/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.8-build.2102+sha.5a9cb8b/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11625,7 +11625,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.8-build.2101+sha.69452fa',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.8-build.2102+sha.5a9cb8b',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 8,
@@ -18524,6 +18524,10 @@ function LocationHashbangUrl(appBase, hashPrefix) {
     this.$$path = removeWindowsDriveName(this.$$path, withoutHashUrl, appBase);
 
     this.$$compose();
+
+    if (!this.$$path) {
+      this.$$path = '/';
+    }
 
     /*
      * In Windows, on an anchor node on documents loaded from
