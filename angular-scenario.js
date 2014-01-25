63e5a11b04a200960cd9da29d93198c04002fdc7
jenkins <ci-bot@angularjs.org>
2014-01-24 16:05:13 -0800

    v1.2.11-build.2182+sha.88a14b4

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.2.10
+ * @license AngularJS v1.2.11-build.2182+sha.88a14b4
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.10/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.11-build.2182+sha.88a14b4/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11626,11 +11626,11 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.10',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.11-build.2182+sha.88a14b4',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
-  dot: 10,
-  codeName: 'augmented-serendipity'
+  dot: 11,
+  codeName: 'cryptocurrency-hyperdeflation'
 };
 
 
