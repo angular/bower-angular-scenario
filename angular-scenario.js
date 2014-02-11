b124017d835cfe645a04a53fbfa419c86dc5bce6
jenkins <ci-bot@angularjs.org>
2014-02-10 16:48:32 -0800

    v1.2.13-build.2246+sha.945fc1a

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.2.13-build.2245+sha.f99fe79
+ * @license AngularJS v1.2.13-build.2246+sha.945fc1a
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.13-build.2245+sha.f99fe79/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.13-build.2246+sha.945fc1a/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11626,7 +11626,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.13-build.2245+sha.f99fe79',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.13-build.2246+sha.945fc1a',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 13,
