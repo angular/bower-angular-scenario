0f41bf79354db3ccf9288910af37fdfa94e290dc
jenkins <ci-bot@angularjs.org>
2014-02-11 16:00:55 -0800

    v1.2.13-build.2253+sha.686b13b

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.2.13-build.2252+sha.56cc7bc
+ * @license AngularJS v1.2.13-build.2253+sha.686b13b
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.13-build.2252+sha.56cc7bc/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.13-build.2253+sha.686b13b/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11003,6 +11003,7 @@ function encodeUriQuery(val, pctEncodeSpaces) {
    <file name="index.html">
    <div ng-controller="ngAppDemoController">
      I can add: {{a}} + {{b}} =  {{ a+b }}
+   </div>
    </file>
    <file name="script.js">
    angular.module('ngAppDemo', []).controller('ngAppDemoController', function($scope) {
@@ -11627,7 +11628,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.13-build.2252+sha.56cc7bc',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.13-build.2253+sha.686b13b',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 13,
