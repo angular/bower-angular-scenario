6fc6a16400ba8d9a48a947ee9e5c6b8a8bf6f56f
jenkins <ci-bot@angularjs.org>
2014-03-02 08:15:59 -0800

    v1.2.15-build.2361+sha.9bffccd

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.2.15-build.2360+sha.6b18a56
+ * @license AngularJS v1.2.15-build.2361+sha.9bffccd
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.15-build.2360+sha.6b18a56/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.15-build.2361+sha.9bffccd/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11672,7 +11672,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.15-build.2360+sha.6b18a56',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.15-build.2361+sha.9bffccd',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 15,
@@ -18369,7 +18369,7 @@ function $IntervalProvider() {
       * @description
       * Cancels a task associated with the `promise`.
       *
-      * @param {number} promise Promise returned by the `$interval` function.
+      * @param {promise} promise returned by the `$interval` function.
       * @returns {boolean} Returns `true` if the task was successfully canceled.
       */
     interval.cancel = function(promise) {
