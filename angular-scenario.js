defd1bd3aa1302d1cc1156be7d5563f392b13d62
jenkins <ci-bot@angularjs.org>
2014-03-02 08:03:02 -0800

    v1.2.15-build.2360+sha.6b18a56

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.2.15-build.2359+sha.27b7fa3
+ * @license AngularJS v1.2.15-build.2360+sha.6b18a56
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.15-build.2359+sha.27b7fa3/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.15-build.2360+sha.6b18a56/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11672,7 +11672,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.15-build.2359+sha.27b7fa3',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.15-build.2360+sha.6b18a56',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 15,
