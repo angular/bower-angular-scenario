bca279fb9749b11038586280a490721d9c7fea8e
jenkins <ci-bot@angularjs.org>
2014-03-13 16:39:31 -0700

    v1.3.0-build.2422+sha.cea44b3

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.3.0-build.2421+sha.e8c6b9b
+ * @license AngularJS v1.3.0-build.2422+sha.cea44b3
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2421+sha.e8c6b9b/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2422+sha.cea44b3/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11670,7 +11670,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.2421+sha.e8c6b9b',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.2422+sha.cea44b3',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: undefined,
@@ -21107,10 +21107,12 @@ function qFactory(nextTick, exceptionHandler) {
 function $$RAFProvider(){ //rAF
   this.$get = ['$window', function($window) {
     var requestAnimationFrame = $window.requestAnimationFrame ||
-                                $window.webkitRequestAnimationFrame;
+                                $window.webkitRequestAnimationFrame ||
+                                $window.mozRequestAnimationFrame;
 
     var cancelAnimationFrame = $window.cancelAnimationFrame ||
-                               $window.webkitCancelAnimationFrame;
+                               $window.webkitCancelAnimationFrame ||
+                               $window.mozCancelAnimationFrame;
 
     var raf = function(fn) {
       var id = requestAnimationFrame(fn);
