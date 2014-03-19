93b6c5346a44aa4d1e935454b7ba958ce5fa1281
jenkins <ci-bot@angularjs.org>
2014-03-18 18:45:56 -0700

    v1.3.0-build.2474+sha.c839f78

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.3.0-build.2473+sha.f7ce415
+ * @license AngularJS v1.3.0-build.2474+sha.c839f78
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2473+sha.f7ce415/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2474+sha.c839f78/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11711,7 +11711,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.2473+sha.f7ce415',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.2474+sha.c839f78',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -21157,7 +21157,8 @@ function $$RAFProvider(){ //rAF
 
     var cancelAnimationFrame = $window.cancelAnimationFrame ||
                                $window.webkitCancelAnimationFrame ||
-                               $window.mozCancelAnimationFrame;
+                               $window.mozCancelAnimationFrame ||
+                               $window.webkitCancelRequestAnimationFrame;
 
     var rafSupported = !!requestAnimationFrame;
     var raf = rafSupported
