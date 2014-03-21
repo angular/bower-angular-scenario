6aac81c1bfc9c78920e6e962a838f24ab85aa7cc
jenkins <ci-bot@angularjs.org>
2014-03-21 15:20:07 -0700

    v1.2.15

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.2.15-build.26+sha.a9b5a10
+ * @license AngularJS v1.2.15
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.15-build.26+sha.a9b5a10/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.15/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11711,11 +11711,11 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.15-build.26+sha.a9b5a10',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.15',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 15,
-  codeName: 'snapshot'
+  codeName: 'beer-underestimating'
 };
 
 
