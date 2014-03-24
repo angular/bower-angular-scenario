3eda66e201bd21fdf45153b542f8e55c7de8fb6c
jenkins <ci-bot@angularjs.org>
2014-03-24 16:10:19 -0700

    v1.3.0-build.2496+sha.76dbb6e

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.3.0-build.2495+sha.4a6d4de
+ * @license AngularJS v1.3.0-build.2496+sha.76dbb6e
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2495+sha.4a6d4de/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2496+sha.76dbb6e/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11711,7 +11711,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.2495+sha.4a6d4de',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.2496+sha.76dbb6e',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -17078,7 +17078,7 @@ function $HttpProvider() {
      *
      * ```
      * module.run(function($http) {
-     *   $http.defaults.headers.common.Authentication = 'Basic YmVlcDpib29w'
+     *   $http.defaults.headers.common.Authorization = 'Basic YmVlcDpib29w'
      * });
      * ```
      *
