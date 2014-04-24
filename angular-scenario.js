e81dac31cbb8e5229ca2eecafe6b2396128ef569
jenkins <ci-bot@angularjs.org>
2014-04-24 16:57:36 -0700

    v1.3.0-build.2634+sha.6ac6621

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.3.0-build.2633+sha.39335d9
+ * @license AngularJS v1.3.0-build.2634+sha.6ac6621
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2633+sha.39335d9/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2634+sha.6ac6621/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11809,7 +11809,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.2633+sha.39335d9',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.2634+sha.6ac6621',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
