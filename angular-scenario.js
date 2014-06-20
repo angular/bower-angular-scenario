7b5d75088b76a5cdbcc68cc0b0ebdabe4336df8b
jenkins <ci-bot@angularjs.org>
2014-06-20 09:10:00 -0700

    v1.3.0-build.2845+sha.e57ad6a

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.3.0-build.2844+sha.dafb8a3
+ * @license AngularJS v1.3.0-build.2845+sha.e57ad6a
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2844+sha.dafb8a3/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.2845+sha.e57ad6a/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11865,7 +11865,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.2844+sha.dafb8a3',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.2845+sha.e57ad6a',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -13347,7 +13347,7 @@ function annotate(fn, strictDi, name) {
 
 
 /**
- * @ngdoc object
+ * @ngdoc service
  * @name $provide
  *
  * @description
