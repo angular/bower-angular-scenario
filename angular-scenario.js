3c6c806a25ad4150fcbc563f9de4b2b48dcb1e65
jenkins <ci-bot@angularjs.org>
2014-09-23 11:06:26 -0700

    v1.3.0-build.3290+sha.fd89975

--- angular-scenario.js
+++ angular-scenario.js
@@ -9190,7 +9190,7 @@ return jQuery;
 }));
 
 /**
- * @license AngularJS v1.3.0-build.3289+sha.d8c8b2e
+ * @license AngularJS v1.3.0-build.3290+sha.fd89975
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9263,7 +9263,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3289+sha.d8c8b2e/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3290+sha.fd89975/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11304,7 +11304,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.3289+sha.d8c8b2e',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.3290+sha.fd89975',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -26537,6 +26537,25 @@ function FormController(element, attrs, $scope, $animate) {
     form.$submitted = false;
     forEach(controls, function(control) {
       control.$setPristine();
+    });
+  };
+
+  /**
+   * @ngdoc method
+   * @name form.FormController#$setUntouched
+   *
+   * @description
+   * Sets the form to its untouched state.
+   *
+   * This method can be called to remove the 'ng-touched' class and set the form controls to their
+   * untouched state (ng-untouched class).
+   *
+   * Setting a form controls back to their untouched state is often useful when setting the form
+   * back to its pristine state.
+   */
+  form.$setUntouched = function () {
+    forEach(controls, function(control) {
+      control.$setUntouched();
     });
   };
 
