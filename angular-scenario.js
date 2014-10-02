e1bd6ce853808fd371bf127ccf6f614e019e473c
jenkins <ci-bot@angularjs.org>
2014-10-02 10:43:52 -0700

    v1.3.0-build.3329+sha.86c7d12

--- angular-scenario.js
+++ angular-scenario.js
@@ -9190,7 +9190,7 @@ return jQuery;
 }));
 
 /**
- * @license AngularJS v1.3.0-build.3328+sha.8da08a1
+ * @license AngularJS v1.3.0-build.3329+sha.86c7d12
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9263,7 +9263,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3328+sha.8da08a1/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3329+sha.86c7d12/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11304,7 +11304,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.3328+sha.8da08a1',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.3329+sha.86c7d12',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -26955,7 +26955,8 @@ var formDirectiveFactory = function(isNgForm) {
 
         return {
           pre: function ngFormPreLink(scope, formElement, attr, controller) {
-            if (!attr.action) {
+            // if `action` attr is not present on the form, prevent the default action (submission)
+            if (!('action' in attr)) {
               // we can't use jq events because if a form is destroyed during submission the default
               // action is not prevented. see #1238
               //
