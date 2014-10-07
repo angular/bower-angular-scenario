fc7291f2176cbeef7b0d1c1ca4cba5ab5612d34f
jenkins <ci-bot@angularjs.org>
2014-10-06 17:30:46 -0700

    v1.3.0-build.3356+sha.e843ae7

--- angular-scenario.js
+++ angular-scenario.js
@@ -9190,7 +9190,7 @@ return jQuery;
 }));
 
 /**
- * @license AngularJS v1.3.0-build.3355+sha.40d8da8
+ * @license AngularJS v1.3.0-build.3356+sha.e843ae7
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9263,7 +9263,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3355+sha.40d8da8/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.3356+sha.e843ae7/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11306,7 +11306,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.3355+sha.40d8da8',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-build.3356+sha.e843ae7',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -15841,37 +15841,35 @@ function $CompileProvider($provide, $$sanitizeUriProvider) {
             var attrEndName = false;
 
             attr = nAttrs[j];
-            if (!msie || msie >= 8 || attr.specified) {
-              name = attr.name;
-              value = trim(attr.value);
+            name = attr.name;
+            value = trim(attr.value);
 
-              // support ngAttr attribute binding
-              ngAttrName = directiveNormalize(name);
-              if (isNgAttr = NG_ATTR_BINDING.test(ngAttrName)) {
-                name = snake_case(ngAttrName.substr(6), '-');
-              }
-
-              var directiveNName = ngAttrName.replace(/(Start|End)$/, '');
-              if (directiveIsMultiElement(directiveNName)) {
-                if (ngAttrName === directiveNName + 'Start') {
-                  attrStartName = name;
-                  attrEndName = name.substr(0, name.length - 5) + 'end';
-                  name = name.substr(0, name.length - 6);
-                }
-              }
-
-              nName = directiveNormalize(name.toLowerCase());
-              attrsMap[nName] = name;
-              if (isNgAttr || !attrs.hasOwnProperty(nName)) {
-                  attrs[nName] = value;
-                  if (getBooleanAttrName(node, nName)) {
-                    attrs[nName] = true; // presence means true
-                  }
-              }
-              addAttrInterpolateDirective(node, directives, value, nName, isNgAttr);
-              addDirective(directives, nName, 'A', maxPriority, ignoreDirective, attrStartName,
-                            attrEndName);
+            // support ngAttr attribute binding
+            ngAttrName = directiveNormalize(name);
+            if (isNgAttr = NG_ATTR_BINDING.test(ngAttrName)) {
+              name = snake_case(ngAttrName.substr(6), '-');
             }
+
+            var directiveNName = ngAttrName.replace(/(Start|End)$/, '');
+            if (directiveIsMultiElement(directiveNName)) {
+              if (ngAttrName === directiveNName + 'Start') {
+                attrStartName = name;
+                attrEndName = name.substr(0, name.length - 5) + 'end';
+                name = name.substr(0, name.length - 6);
+              }
+            }
+
+            nName = directiveNormalize(name.toLowerCase());
+            attrsMap[nName] = name;
+            if (isNgAttr || !attrs.hasOwnProperty(nName)) {
+                attrs[nName] = value;
+                if (getBooleanAttrName(node, nName)) {
+                  attrs[nName] = true; // presence means true
+                }
+            }
+            addAttrInterpolateDirective(node, directives, value, nName, isNgAttr);
+            addDirective(directives, nName, 'A', maxPriority, ignoreDirective, attrStartName,
+                          attrEndName);
           }
 
           // use class as directive
@@ -23257,12 +23255,9 @@ function $$SanitizeUriProvider() {
     return function sanitizeUri(uri, isImage) {
       var regex = isImage ? imgSrcSanitizationWhitelist : aHrefSanitizationWhitelist;
       var normalizedVal;
-      // NOTE: urlResolve() doesn't support IE < 8 so we don't sanitize for that case.
-      if (!msie || msie >= 8 ) {
-        normalizedVal = urlResolve(uri).href;
-        if (normalizedVal !== '' && !normalizedVal.match(regex)) {
-          return 'unsafe:'+normalizedVal;
-        }
+      normalizedVal = urlResolve(uri).href;
+      if (normalizedVal !== '' && !normalizedVal.match(regex)) {
+        return 'unsafe:'+normalizedVal;
       }
       return uri;
     };
@@ -26061,22 +26056,6 @@ function ngDirective(directive) {
 var htmlAnchorDirective = valueFn({
   restrict: 'E',
   compile: function(element, attr) {
-
-    if (msie <= 8) {
-
-      // turn <a href ng-click="..">link</a> into a stylable link in IE
-      // but only if it doesn't have name attribute, in which case it's an anchor
-      if (!attr.href && !attr.name) {
-        attr.$set('href', '');
-      }
-
-      // add a comment node to anchors to workaround IE bug that causes element content to be reset
-      // to new attribute content if attribute is updated with value containing @ and element also
-      // contains value with @
-      // see issue #1949
-      element.append(document.createComment('IE fix'));
-    }
-
     if (!attr.href && !attr.xlinkHref && !attr.name) {
       return function(scope, element) {
         // SVGAElement does not use the href attribute, but rather the 'xlinkHref' attribute.
@@ -34578,97 +34557,71 @@ _jQuery.fn.bindings = function(windowJquery, bindExp) {
       return keys.indexOf(key) !== -1;
     }
 
-    if (msie < 9) {
-      if (inputType == 'radio' || inputType == 'checkbox') {
-          element.checked = !element.checked;
-      }
-
-      // WTF!!! Error: Unspecified error.
-      // Don't know why, but some elements when detached seem to be in inconsistent state and
-      // calling .fireEvent() on them will result in very unhelpful error (Error: Unspecified error)
-      // forcing the browser to compute the element position (by reading its CSS)
-      // puts the element in consistent state.
-      element.style.posLeft;
-
-      // TODO(vojta): create event objects with pressed keys to get it working on IE<9
-      var ret = element.fireEvent('on' + eventType);
-      if (inputType == 'submit') {
-        while(element) {
-          if (element.nodeName.toLowerCase() == 'form') {
-            element.fireEvent('onsubmit');
-            break;
-          }
-          element = element.parentNode;
-        }
-      }
-      return ret;
-    } else {
-      var evnt;
-      if(/transitionend/.test(eventType)) {
-        if(window.WebKitTransitionEvent) {
-          evnt = new WebKitTransitionEvent(eventType, eventData);
-          evnt.initEvent(eventType, false, true);
-        }
-        else {
-          try {
-            evnt = new TransitionEvent(eventType, eventData);
-          }
-          catch(e) {
-            evnt = document.createEvent('TransitionEvent');
-            evnt.initTransitionEvent(eventType, null, null, null, eventData.elapsedTime || 0);
-          }
-        }
-      }
-      else if(/animationend/.test(eventType)) {
-        if(window.WebKitAnimationEvent) {
-          evnt = new WebKitAnimationEvent(eventType, eventData);
-          evnt.initEvent(eventType, false, true);
-        }
-        else {
-          try {
-            evnt = new AnimationEvent(eventType, eventData);
-          }
-          catch(e) {
-            evnt = document.createEvent('AnimationEvent');
-            evnt.initAnimationEvent(eventType, null, null, null, eventData.elapsedTime || 0);
-          }
-        }
+    var evnt;
+    if(/transitionend/.test(eventType)) {
+      if(window.WebKitTransitionEvent) {
+        evnt = new WebKitTransitionEvent(eventType, eventData);
+        evnt.initEvent(eventType, false, true);
       }
       else {
-        evnt = document.createEvent('MouseEvents');
-        x = x || 0;
-        y = y || 0;
-        evnt.initMouseEvent(eventType, true, true, window, 0, x, y, x, y, pressed('ctrl'),
-            pressed('alt'), pressed('shift'), pressed('meta'), 0, element);
+        try {
+          evnt = new TransitionEvent(eventType, eventData);
+        }
+        catch(e) {
+          evnt = document.createEvent('TransitionEvent');
+          evnt.initTransitionEvent(eventType, null, null, null, eventData.elapsedTime || 0);
+        }
       }
-
-      /* we're unable to change the timeStamp value directly so this
-       * is only here to allow for testing where the timeStamp value is
-       * read */
-      evnt.$manualTimeStamp = eventData.timeStamp;
-
-      if(!evnt) return;
-
-      var originalPreventDefault = evnt.preventDefault,
-          appWindow = element.ownerDocument.defaultView,
-          fakeProcessDefault = true,
-          finalProcessDefault,
-          angular = appWindow.angular || {};
-
-      // igor: temporary fix for https://bugzilla.mozilla.org/show_bug.cgi?id=684208
-      angular['ff-684208-preventDefault'] = false;
-      evnt.preventDefault = function() {
-        fakeProcessDefault = false;
-        return originalPreventDefault.apply(evnt, arguments);
-      };
-
-      element.dispatchEvent(evnt);
-      finalProcessDefault = !(angular['ff-684208-preventDefault'] || !fakeProcessDefault);
-
-      delete angular['ff-684208-preventDefault'];
-
-      return finalProcessDefault;
     }
+    else if(/animationend/.test(eventType)) {
+      if(window.WebKitAnimationEvent) {
+        evnt = new WebKitAnimationEvent(eventType, eventData);
+        evnt.initEvent(eventType, false, true);
+      }
+      else {
+        try {
+          evnt = new AnimationEvent(eventType, eventData);
+        }
+        catch(e) {
+          evnt = document.createEvent('AnimationEvent');
+          evnt.initAnimationEvent(eventType, null, null, null, eventData.elapsedTime || 0);
+        }
+      }
+    }
+    else {
+      evnt = document.createEvent('MouseEvents');
+      x = x || 0;
+      y = y || 0;
+      evnt.initMouseEvent(eventType, true, true, window, 0, x, y, x, y, pressed('ctrl'),
+          pressed('alt'), pressed('shift'), pressed('meta'), 0, element);
+    }
+
+    /* we're unable to change the timeStamp value directly so this
+     * is only here to allow for testing where the timeStamp value is
+     * read */
+    evnt.$manualTimeStamp = eventData.timeStamp;
+
+    if(!evnt) return;
+
+    var originalPreventDefault = evnt.preventDefault,
+        appWindow = element.ownerDocument.defaultView,
+        fakeProcessDefault = true,
+        finalProcessDefault,
+        angular = appWindow.angular || {};
+
+    // igor: temporary fix for https://bugzilla.mozilla.org/show_bug.cgi?id=684208
+    angular['ff-684208-preventDefault'] = false;
+    evnt.preventDefault = function() {
+      fakeProcessDefault = false;
+      return originalPreventDefault.apply(evnt, arguments);
+    };
+
+    element.dispatchEvent(evnt);
+    finalProcessDefault = !(angular['ff-684208-preventDefault'] || !fakeProcessDefault);
+
+    delete angular['ff-684208-preventDefault'];
+
+    return finalProcessDefault;
   };
 }());
 
