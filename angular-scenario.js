4c8a0a9ddaa92e75ecba8e05c751281eb49b6d2d
jenkins <ci-bot@angularjs.org>
2014-10-22 07:37:11 -0700

    v1.2.27-build.530+sha.efedc64

--- angular-scenario.js
+++ angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.2.27-build.529+sha.34230b3
+ * @license AngularJS v1.2.27-build.530+sha.efedc64
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.2.27-build.529+sha.34230b3/' +
+    message = message + '\nhttp://errors.angularjs.org/1.2.27-build.530+sha.efedc64/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11779,7 +11779,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.2.27-build.529+sha.34230b3',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.2.27-build.530+sha.efedc64',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 2,
   dot: 27,
@@ -14906,7 +14906,8 @@ function $CacheFactoryProvider() {
  * ```
  *
  * **Note:** the `script` tag containing the template does not need to be included in the `head` of
- * the document, but it must be below the `ng-app` definition.
+ * the document, but it must be a descendent of the {@link ng.$rootElement $rootElement} (IE,
+ * element with ng-app attribute), otherwise the template will be ignored.
  *
  * Adding via the $templateCache service:
  *
