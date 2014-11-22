fdebc8c68b655bc49520628520dea85c50146712
jenkins <ci-bot@angularjs.org>
2014-11-22 06:47:05 -0800

    v1.3.4-build.3599+sha.891acf4

--- angular-scenario.js
+++ angular-scenario.js
@@ -9190,7 +9190,7 @@ return jQuery;
 }));
 
 /**
- * @license AngularJS v1.3.4-build.3598+sha.b5fbd6a
+ * @license AngularJS v1.3.4-build.3599+sha.891acf4
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9246,7 +9246,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.4-build.3598+sha.b5fbd6a/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.4-build.3599+sha.891acf4/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -11292,7 +11292,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.4-build.3598+sha.b5fbd6a',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.4-build.3599+sha.891acf4',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 4,
