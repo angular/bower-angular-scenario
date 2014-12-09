f7dc62c1baa5ecb79c371e365f9ac0ca5cd4073d
jenkins <ci-bot@angularjs.org>
2014-12-08 16:40:52 -0800

    v1.3.7-build.3682+sha.ca4df47

--- angular-scenario.js
+++ angular-scenario.js
@@ -9190,7 +9190,7 @@ return jQuery;
 }));
 
 /**
- * @license AngularJS v1.3.6
+ * @license AngularJS v1.3.7-build.3682+sha.ca4df47
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9246,7 +9246,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.6/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.7-build.3682+sha.ca4df47/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -11297,11 +11297,11 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.6',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.7-build.3682+sha.ca4df47',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
-  dot: 6,
-  codeName: 'robofunky-danceblaster'
+  dot: 7,
+  codeName: 'snapshot'
 };
 
 
