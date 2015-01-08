79f6790eb48eb17478b96162777562fb2e565976
jenkins <ci-bot@angularjs.org>
2015-01-08 11:44:53 -0800

    v1.3.9-build.3760+sha.ef1a9d2

--- angular-scenario.js
+++ angular-scenario.js
@@ -9190,7 +9190,7 @@ return jQuery;
 }));
 
 /**
- * @license AngularJS v1.3.9-build.3758+sha.9c9c6b3
+ * @license AngularJS v1.3.9-build.3760+sha.ef1a9d2
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9246,7 +9246,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.3758+sha.9c9c6b3/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.9-build.3760+sha.ef1a9d2/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i - 2) + '=' +
@@ -11310,7 +11310,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.9-build.3758+sha.9c9c6b3',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.9-build.3760+sha.ef1a9d2',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 9,
@@ -32839,7 +32839,7 @@ var ngModelOptionsDirective = function() {
     restrict: 'A',
     controller: ['$scope', '$attrs', function($scope, $attrs) {
       var that = this;
-      this.$options = angular.copy($scope.$eval($attrs.ngModelOptions));
+      this.$options = copy($scope.$eval($attrs.ngModelOptions));
       // Allow adding/overriding bound events
       if (this.$options.updateOn !== undefined) {
         this.$options.updateOnDefault = false;
