0db5a71912c2e84ad9ac9216ba1babe474fd16d9
jenkins <ci-bot@angularjs.org>
2015-02-18 05:59:21 -0800

    v1.4.0-build.3841+sha.ef894c8

--- angular-scenario.js
+++ angular-scenario.js
@@ -9190,7 +9190,7 @@ return jQuery;
 }));
 
 /**
- * @license AngularJS v1.4.0-build.3840+sha.30b4813
+ * @license AngularJS v1.4.0-build.3841+sha.ef894c8
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9249,7 +9249,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.4.0-build.3840+sha.30b4813/' +
+    message += '\nhttp://errors.angularjs.org/1.4.0-build.3841+sha.ef894c8/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -11387,7 +11387,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.4.0-build.3840+sha.30b4813',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.4.0-build.3841+sha.ef894c8',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 4,
   dot: 0,
@@ -34265,7 +34265,7 @@ var ngOptionsDirective = ['$compile', '$parse', function($compile, $parse) {
   return {
     restrict: 'A',
     terminal: true,
-    require: ['select', 'ngModel'],
+    require: ['select', '?ngModel'],
     link: function(scope, selectElement, attr, ctrls) {
 
       // if ngModel is not defined, we don't need to do anything
